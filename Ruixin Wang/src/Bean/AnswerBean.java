38d26381a7b17b9555ceff2be99ac1c02063ce47
Warren981026 <63449791+Warren981026@users.noreply.github.com>
2020-05-28 15:16:18 +0800

    Add files via upload

---
+++ Ruixin Wang/src/Bean/AnswerBean.java
@@ -0,0 +1,74 @@
+package Bean;
+
+public class AnswerBean {
+	public int index;
+	public int questionnaireID;
+	public String submitTime;
+	public int timeForWriting;
+	public int feedback;
+	public String content;
+	public int status;
+	public int userID;
+	public AnswerBean(int index, int questionnaireID, String submitTime, int timeForWriting, int feedback,
+			String content, int status, int userID) {
+		super();
+		this.index = index;
+		this.questionnaireID = questionnaireID;
+		this.submitTime = submitTime;
+		this.timeForWriting = timeForWriting;
+		this.feedback = feedback;
+		this.content = content;
+		this.status = status;
+		this.userID = userID;
+	}
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	public int getQuestionnaireID() {
+		return questionnaireID;
+	}
+	public void setQuestionnaireID(int questionnaireID) {
+		this.questionnaireID = questionnaireID;
+	}
+	public String getSubmitTime() {
+		return submitTime;
+	}
+	public void setSubmitTime(String submitTime) {
+		this.submitTime = submitTime;
+	}
+	public int getTimeForWriting() {
+		return timeForWriting;
+	}
+	public void setTimeForWriting(int timeForWriting) {
+		this.timeForWriting = timeForWriting;
+	}
+	public int getFeedback() {
+		return feedback;
+	}
+	public void setFeedback(int feedback) {
+		this.feedback = feedback;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	
+	
+}
---
+++ Ruixin Wang/src/Bean/QuestionBean.java
@@ -0,0 +1,85 @@
+package Bean;
+
+public class QuestionBean {
+	public int questionnaireID;
+	public int userID;
+	public String title;
+	public String content;
+	public int status;
+	public String publishTime;
+	public String tag1;
+	public String tag2;
+	public int cnt;
+	
+	public QuestionBean(int questionnaireID, int userID, String title, String content, int status, String publishTime,
+			String tag1, String tag2, int cnt) {
+		super();
+		this.questionnaireID = questionnaireID;
+		this.userID = userID;
+		this.title = title;
+		this.content = content;
+		this.status = status;
+		this.publishTime = publishTime;
+		this.tag1 = tag1;
+		this.tag2 = tag2;
+		this.cnt = cnt;
+	}
+	public int getQuestionnaireID() {
+		return questionnaireID;
+	}
+	public void setQuestionnaireID(int questionnaireID) {
+		this.questionnaireID = questionnaireID;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getPublishTime() {
+		return publishTime;
+	}
+	public void setPublishTime(String publishTime) {
+		this.publishTime = publishTime;
+	}
+	public String getTag1() {
+		return tag1;
+	}
+	public void setTag1(String tag1) {
+		this.tag1 = tag1;
+	}
+	public String getTag2() {
+		return tag2;
+	}
+	public void setTag2(String tag2) {
+		this.tag2 = tag2;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
+	
+	
+
+}
